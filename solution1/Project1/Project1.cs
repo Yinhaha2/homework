a84efd8d2c14e1fcd7a83f38989e93dcefff23ac
Yinhaha2 <121076506+Yinhaha2@users.noreply.github.com>
2023-03-07 22:53:02 +0800

    Add files via upload

---
+++ solution1/Project1/Project1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+namespace ConsoleApp1
+{
+   class Project1
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt=new DataTable();
+            string a,b,c;   
+            Console.WriteLine( "输入一个数字" );
+            a=Console.ReadLine();
+            Console.WriteLine("输入运算符");
+            b=Console.ReadLine();
+            Console.WriteLine("输入第二个数字");
+            c=Console.ReadLine();
+            string d = a+b+c;
+            int e =(int) dt.Compute(d,"");
+            Console.WriteLine(  "结果为",e );
+            Console.ReadLine();
+        }
+    }
+}
